9d1f5e1755481f62c4689d8f95a1af88b3cfd9a8
Vladimír Urík <gggedrvideos@gmail.com>
2022-11-06 12:05:46 +0100

    Fix issue with unlimited memory

--- src/main/kotlin/tech/goksi/pterobot/util/MemoryBar.kt
+++ src/main/kotlin/tech/goksi/pterobot/util/MemoryBar.kt
@@ -7,15 +7,23 @@ data class MemoryBar(val usedMemory: Long, val maxMemory: Long) {
 
     override fun toString(): String {
         val builder = StringBuilder()
-        val oneBlock = (maxMemory / 10).toInt()
-        val numberOfFull = (usedMemory / oneBlock).toInt()
-
-        for (i in 1..10) {
-            if (i <= numberOfFull) {
-                builder.append(FULL_CHAR)
-            } else {
+        if(maxMemory < 1) {
+            builder.append(FULL_CHAR)
+            for (i in 1..9) {
                 builder.append(EMPTY_CHAR)
             }
+        } else {
+            val oneBlock = (maxMemory / 10).toInt()
+            val numberOfFull = (usedMemory / oneBlock).toInt()
+
+            for (i in 1..10) {
+                if (i <= numberOfFull) {
+                    builder.append(FULL_CHAR)
+                } else {
+                    builder.append(EMPTY_CHAR)
+                }
+            }
+
         }
         return builder.toString()
     }
