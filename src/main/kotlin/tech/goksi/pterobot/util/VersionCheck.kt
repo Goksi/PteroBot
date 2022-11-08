528711b93c77ecbd56d06e6b0096a9f1207045c8
Vukašin Lekić <goksi1312@gmail.com>
2022-11-08 01:32:55 +0100

    Update VersionCheck.kt

--- src/main/kotlin/tech/goksi/pterobot/util/VersionCheck.kt
+++ src/main/kotlin/tech/goksi/pterobot/util/VersionCheck.kt
@@ -3,17 +3,18 @@ package tech.goksi.pterobot.util
 import dev.minn.jda.ktx.util.SLF4J
 import net.dv8tion.jda.api.utils.data.DataObject
 import okhttp3.*
-import tech.goksi.pterobot.manager.EmbedManager.replace
 import java.io.IOException
+import java.util.Properties
 
 object VersionCheck {
     private val logger by SLF4J
 
     private const val API_URL = "https://api.github.com/repos/Goksi/PteroBot/releases/latest"
-    private const val VERSION = "@VERSION@"
 
     fun checkVersion() {
-        //val currentVer = VERSION.replace(".", "").toInt()
+        val properties = Properties()
+            .apply { this.load(VersionCheck::class.java.classLoader.getResourceAsStream("version.properties")) }
+        val currentVer = properties.getProperty("version")!!
         val client = OkHttpClient()
         val request = Request.Builder()
             .url(API_URL)
@@ -30,10 +31,31 @@ object VersionCheck {
                     if (!response.isSuccessful) logger.error("Error while requesting version, unexpected status code: $response")
                     val fetchedVer = DataObject.fromJson(response.body!!.byteStream())
                         .getString("tag_name")
-                        .replace("v" to "", "." to "").toInt()
-                    logger.info(fetchedVer.toString())
+
+                    if (compareVersions(fetchedVer.replace("v", ""), currentVer) > 0)
+                        logger.warn("You are not running latest version of PteroBot ! Latest: $fetchedVer Current: v$currentVer")
+                    else logger.info("You are running latest version of PteroBot !")
                 }
+
             }
         })
+    }
+
+    private fun compareVersions(version1: String, version2: String): Int {
+        var result = 0
+
+        val ver1List = version1.split(".")
+        val ver2List = version2.split(".")
+
+        for (i in 0..2) {
+            val v1 = ver1List[i].toInt()
+            val v2 = ver2List[i].toInt()
+            val compare = v1.compareTo(v2)
+            if(compare != 0){
+                result = compare
+                break;
+            }
+        }
+        return result
     }
 }
