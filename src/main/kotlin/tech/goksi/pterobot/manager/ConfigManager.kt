0491ced2b7d7ebf9035d60b1dd4a1214ce5feb2b
Vukašin Lekić <goksi1312@gmail.com>
2022-11-13 14:02:12 +0100

    Loads default from resources if no value in config in present

--- src/main/kotlin/tech/goksi/pterobot/manager/ConfigManager.kt
+++ src/main/kotlin/tech/goksi/pterobot/manager/ConfigManager.kt
@@ -1,24 +1,29 @@
 package tech.goksi.pterobot.manager
 
 import dev.minn.jda.ktx.util.SLF4J
+import org.simpleyaml.configuration.file.YamlConfiguration
 import org.simpleyaml.configuration.file.YamlFile
+import org.simpleyaml.utils.SupplierIO
 import java.io.File
 import java.io.IOException
 import kotlin.system.exitProcess
-/*TODO: handle new config options*/
+
 object ConfigManager {
     private val configFile: File = File("config.yml")
     private val logger by SLF4J
     val config: YamlFile
 
     init {
+        val inputStreamURL = ConfigManager::class.java.classLoader.getResource("config.yml")!!
         if (!configFile.exists()) {
-            val content = ConfigManager::class.java.classLoader.getResource("config.yml")!!.readText()
+            val content = inputStreamURL.readText()
             configFile.writeText(content)
         }
         config = YamlFile(configFile)
         try {
             config.loadWithComments()
+            config.defaults =
+                YamlConfiguration.loadConfiguration(SupplierIO.InputStream { inputStreamURL.openStream() })
         } catch (exception: IOException) {
             logger.error("Error while loading configuration file, exiting program...", exception)
             exitProcess(1)
