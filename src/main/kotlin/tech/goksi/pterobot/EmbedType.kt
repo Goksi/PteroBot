8aa3d40365512ed3438b89418b9a70c711c5a7f6
Vukašin Lekić <goksi1312@gmail.com>
2022-11-17 13:02:34 +0100

    Update EmbedType.kt

--- src/main/kotlin/tech/goksi/pterobot/EmbedType.kt
+++ src/main/kotlin/tech/goksi/pterobot/EmbedType.kt
@@ -1,16 +1,14 @@
 package tech.goksi.pterobot
 
-enum class EmbedType(val path: String) {
-    GENERIC_ERROR("embeds/generic_error.json"),
-    GENERIC_SUCCESS("embeds/generic_success.json"),
-    NODE_INFO("embeds/node_info.json"),
-    SERVERS_COMMAND("embeds/servers_command.json"),
-    SERVER_INFO("embeds/server_info.json"),
-    NODE_STATUS("embeds/node_status.json");
+enum class EmbedType(private val fileName: String) {
+    GENERIC_ERROR("generic_error.json"),
+    GENERIC_SUCCESS("generic_success.json"),
+    NODE_INFO("node_info.json"),
+    SERVERS_COMMAND("servers_command.json"),
+    SERVER_INFO("server_info.json"),
+    NODE_STATUS("node_status.json");
 
-
-    override fun toString(): String {
-        return path
-    }
+    val path
+        get() = "embeds/$fileName"
 
 }
