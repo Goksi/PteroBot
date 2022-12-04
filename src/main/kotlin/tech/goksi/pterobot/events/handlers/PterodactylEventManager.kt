81332a6819f449ceb579668de5f988c750ebad9a
Vukašin Lekić <goksi1312@gmail.com>
2022-12-04 14:17:43 +0100

    Create PterodactylEventManager.kt

---
+++ src/main/kotlin/tech/goksi/pterobot/events/handlers/PterodactylEventManager.kt
@@ -0,0 +1,22 @@
+package tech.goksi.pterobot.events.handlers
+
+import com.mattmalec.pterodactyl4j.client.ws.events.Event
+import com.mattmalec.pterodactyl4j.client.ws.hooks.IClientListenerManager
+
+class PterodactylEventManager: IClientListenerManager {
+    override fun register(listener: Any?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun unregister(listener: Any?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getRegisteredListeners(): MutableList<Any> {
+        TODO("Not yet implemented")
+    }
+
+    override fun handle(event: Event?) {
+        TODO("Not yet implemented")
+    }
+}
