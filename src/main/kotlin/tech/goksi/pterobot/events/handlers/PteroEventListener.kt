e2b2b3a4e355cee782d9bc74233b0cc9f0efb158
Vukašin Lekić <goksi1312@gmail.com>
2022-12-06 00:58:58 +0100

    Refactor

--- src/main/kotlin/tech/goksi/pterobot/events/handlers/PteroEventListener.kt
+++ src/main/kotlin/tech/goksi/pterobot/events/handlers/PteroEventListener.kt
@@ -4,7 +4,7 @@ import com.mattmalec.pterodactyl4j.client.ws.events.Event
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
 
-interface PterodactylEventListener {
+interface PteroEventListener {
     val expireAfter: Duration get() = 2.seconds // 2 seconds fixed for expiration
 
     suspend fun onEvent(event: Event)
--- src/main/kotlin/tech/goksi/pterobot/events/handlers/PteroEventManager.kt
+++ src/main/kotlin/tech/goksi/pterobot/events/handlers/PteroEventManager.kt
@@ -1,29 +1,26 @@
 package tech.goksi.pterobot.events.handlers
 
-import com.mattmalec.pterodactyl4j.client.entities.ClientServer
 import com.mattmalec.pterodactyl4j.client.managers.WebSocketManager
 import com.mattmalec.pterodactyl4j.client.ws.events.AuthSuccessEvent
 import com.mattmalec.pterodactyl4j.client.ws.events.Event
-import com.mattmalec.pterodactyl4j.client.ws.events.output.ConsoleOutputEvent
+import com.mattmalec.pterodactyl4j.client.ws.events.output.OutputEvent
 import com.mattmalec.pterodactyl4j.client.ws.hooks.ClientSocketListener
 import com.mattmalec.pterodactyl4j.client.ws.hooks.IClientListenerManager
 import dev.minn.jda.ktx.util.SLF4J
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withTimeoutOrNull
 import okhttp3.internal.toImmutableList
 import tech.goksi.pterobot.util.Common
-import java.lang.StringBuilder
 import java.util.concurrent.CopyOnWriteArrayList
 
-class PterodactylEventManager : IClientListenerManager {
+class PteroEventManager : IClientListenerManager {
     private val logger by SLF4J
     private val scope = Common.getDefaultCoroutineScope()
     private val listeners = CopyOnWriteArrayList<Any>()
     override fun register(listener: Any) {
         listeners.add(
             when (listener) {
-                is PterodactylEventListener, is ClientSocketListener -> listener
+                is PteroEventListener, is ClientSocketListener -> listener
                 else -> throw IllegalArgumentException("Wrong implementation of listener !")
             }
         )
@@ -40,7 +37,7 @@ class PterodactylEventManager : IClientListenerManager {
     override fun handle(event: Event) {
         scope.launch {
             for (listener in listeners) try {
-                if (listener is PterodactylEventListener) {
+                if (listener is PteroEventListener) {
                     if (listener.expireAfter.isPositive() && listener.expireAfter.isFinite()) {
                         val result = withTimeoutOrNull(listener.expireAfter.inWholeMilliseconds) {
                             listener.onEvent(event)
@@ -56,25 +53,14 @@ class PterodactylEventManager : IClientListenerManager {
         }
     }
 
-    inline fun websocketListener(crossinline consumer: suspend PterodactylEventListener.(ConsoleOutputEvent) -> Unit): PterodactylEventListener {
-        return object : PterodactylEventListener {
+    inline fun websocketListener(crossinline consumer: (OutputEvent) -> Unit): PteroEventListener {
+        return object : PteroEventListener {
             override suspend fun onEvent(event: Event) {
                 if (event is AuthSuccessEvent) event.webSocketManager.request(WebSocketManager.RequestAction.LOGS)
-                else if (event is ConsoleOutputEvent) consumer(event)
+                else if (event is OutputEvent) consumer(event)
             }
 
             override fun cancel() = unregister(this)
-        }.also { register(it) }
+        }
     }
-}
-
-suspend fun ClientServer.getLogs(): String {
-    val stringJoiner = StringBuilder()
-    val handler = PterodactylEventManager()
-    val task = handler.websocketListener { stringJoiner.append(it.line).append('\n'); }
-    val wss = this.webSocketBuilder.addEventListeners(task).build()
-    delay(task.expireAfter)
-    task.cancel()
-    wss.shutdown()
-    return stringJoiner.toString()
 }
