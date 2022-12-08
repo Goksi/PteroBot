7075485fa90650ff79fb3cac51c15a606d53aeee
Vukašin Lekić <goksi1312@gmail.com>
2022-12-08 23:10:54 +0100

    Update Common.kt

--- src/main/kotlin/tech/goksi/pterobot/util/Common.kt
+++ src/main/kotlin/tech/goksi/pterobot/util/Common.kt
@@ -14,6 +14,7 @@ object Common {
     private val logger by SLF4J<PteroEventManager>()
 
     val ansiRegex by lazy { ">?\u001B\\[[\\d;]*[^\\d;]".toRegex() }
+    val newLineRegex by lazy { "\r?\n?".toRegex() }
     fun createClient(apiKey: String?): PteroClient? {
         if (apiKey == null) return null
         val appUrl = ConfigManager.config.getString("BotInfo.PterodactylUrl")
@@ -54,10 +55,9 @@ object Common {
         val stringBuilder = StringBuilder()
         val wssBuilder = this.webSocketBuilder
         val task = (wssBuilder.eventManager as PteroEventManager).websocketListener {
-            val line = it.line
-            if (line.isEmpty()) return@websocketListener
-            if (!line.contains('\r') && stringBuilder.isNotEmpty()) stringBuilder.append('\n')
-            stringBuilder.append(line)
+            val line = it.line.replace(newLineRegex, "")
+            if(line.isEmpty()) return@websocketListener
+            stringBuilder.append(line).append('\n')
         }
         val wss = wssBuilder.addEventListeners(task).build()
         delay(task.expireAfter)
