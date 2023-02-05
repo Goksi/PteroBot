cc82ce3f675956bbad72f91e8c1785c2be83d505
Vukašin Lekić <goksi1312@gmail.com>
2023-02-05 02:23:22 +0100

    Temp fix for multi event call, don't know cause yet

--- src/main/kotlin/tech/goksi/pterobot/events/hook/CoroutineListenerAdapter.kt
+++ src/main/kotlin/tech/goksi/pterobot/events/hook/CoroutineListenerAdapter.kt
@@ -2,6 +2,7 @@ package tech.goksi.pterobot.events.hook
 
 import dev.minn.jda.ktx.events.CoroutineEventListener
 import net.dv8tion.jda.api.events.GenericEvent
+import net.dv8tion.jda.api.events.interaction.GenericInteractionCreateEvent
 import net.dv8tion.jda.api.events.interaction.ModalInteractionEvent
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent
@@ -16,7 +17,11 @@ open class CoroutineListenerAdapter : CoroutineEventListener {
 
     open suspend fun onGenericEvent(event: GenericEvent) {}
 
+    /*TODO: temp fix, find why every event inside of server list is executed twice is executed twice*/
     override suspend fun onEvent(event: GenericEvent) {
+        if (event is GenericInteractionCreateEvent) {
+            if (event.isAcknowledged) return
+        }
         onGenericEvent(event)
         when (event) {
             is SlashCommandInteractionEvent -> onSlashCommandInteraction(event)
