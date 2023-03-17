90325ec62ec0c8227375d7c87582188c523b1846
Magnetite <35513713+MagnetiteMC@users.noreply.github.com>
2023-03-17 09:48:16 -0400

    Fix incorrect config path

--- src/main/kotlin/tech/goksi/pterobot/NodeStatus.kt
+++ src/main/kotlin/tech/goksi/pterobot/NodeStatus.kt
@@ -2,7 +2,7 @@ package tech.goksi.pterobot
 
 import tech.goksi.pterobot.manager.ConfigManager
 
-private const val CONFIG_PREFIX = "Messages.Commands.NodeInfo."
+private const val CONFIG_PREFIX = "Messages.Commands.Node.Info."
 
 enum class NodeStatus(val message: String, val emoji: String) {
     ONLINE("Online", ConfigManager.config.getString(CONFIG_PREFIX + "OnlineEmoji")),
